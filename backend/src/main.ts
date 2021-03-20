93ad37be25bea1ae73ae0e45f8254a6d37a2c557
edmar <edsiqueira73@gmail.com>
2021-03-20 02:50:04 -0300

    feat: enable cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,6 +5,7 @@ import { PORT } from './constants/config'
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule)
+	app.enableCors()
 	app.useGlobalPipes(new ValidationPipe())
 	await app.listen(PORT)
 }
