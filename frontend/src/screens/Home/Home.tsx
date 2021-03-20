2a0d23d9fb7f1a409208e0cedc75107f7f85899a
edmar <edsiqueira73@gmail.com>
2021-03-20 09:50:01 -0300

    feat: get books from api

--- frontend/src/screens/Home/Home.tsx
+++ frontend/src/screens/Home/Home.tsx
@@ -1,18 +1,33 @@
+import { Grid } from '@material-ui/core'
 import { Book } from 'components/Book'
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useStoreon } from 'storeon/react'
 
 export interface HomeProps {}
 
-const book = {
-	name: 'Brizas Noturnas',
+const bookInput = {
+	name: 'Brizas Noturnasaa',
 	author: 'Edmar Junior',
 	editor: 'Greg Teixeira',
 	category: 'Brizas',
 	quantity: 1010
 }
 
-export const Home = (props: HomeProps) => (
-	<div>
-		<Book {...book} />
-	</div>
-)
+export const Home = (props: HomeProps) => {
+	const { dispatch, books } = useStoreon('books')
+	useEffect(() => {
+		setTimeout(() => {
+			dispatch('books/create', bookInput)
+		}, 3000)
+		// eslint-disable-next-line
+	}, [])
+	return (
+		<Grid container spacing={3}>
+			{books.map(({ id, ...book }) => (
+				<Grid item key={id}>
+					<Book {...book} />
+				</Grid>
+			))}
+		</Grid>
+	)
+}
